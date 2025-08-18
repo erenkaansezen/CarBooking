819e7ca7e40d914dca0c71536dd9b742db8e9872
Eren Sezen <erenkaansez17@gmail.com>
2025-08-18 16:55:00 +0000

    addEntities

---
+++ CarBooking/Web.Domain/Entities/FooterAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Domain.Entities
+{
+    public class FooterAddress
+    {
+        public int FooterAddressID { get; set; }
+        public int Description { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
