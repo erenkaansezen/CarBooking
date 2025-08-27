671bbf7c50af2711ba2d9b05462cb32133b53035
Eren Sezen <erenkaansez17@gmail.com>
2025-08-27 12:35:58 +0000

    fixedPricingAPI

--- CarBooking/WebAPI/Controllers/PricingController.cs
+++ CarBooking/WebAPI/Controllers/PricingController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.Features.Mediator.Commands.PricingCommands;
+using Application.Features.Mediator.Queries.PricingQueries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PricingController : ControllerBase
+    public class PricingsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public PricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPricings()
+        {
+            var result = await _mediator.Send(new GetPricingQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPricingById(int id)
+        {
+            var value = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing güncellendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok("Pricing silindi");
+        }
     }
 }
