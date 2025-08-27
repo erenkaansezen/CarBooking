a07e3233f57a1703ee36513b9c4652c840d85a12
Eren Sezen <erenkaansez17@gmail.com>
2025-08-27 20:23:15 +0000

    addBecomeDriverComponent

--- CarBooking/Web.UI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ CarBooking/Web.UI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -2,30 +2,28 @@
 using Newtonsoft.Json;
 using Web.Dto.AboutDtos;
 
-namespace Web.UI.ViewComponents.AboutViewComponents
+namespace Web.UI.ViewComponents.TestimonialViewComponents
 {
-    public class _AboutUsComponentPartial : ViewComponent
+    public class _TestimonialComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> Invoke()
         {
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("https://localhost:7245/api/Abouts");
+            var response = await client.GetAsync("https://localhost:7245/api/Testimonials");
             if (response.IsSuccessStatusCode)
             {
                 var jsonData = await response.Content.ReadAsStringAsync();
-                var values=JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
                 return View(values);
             }
             return View();
         }
     }
-    
-    
 }
