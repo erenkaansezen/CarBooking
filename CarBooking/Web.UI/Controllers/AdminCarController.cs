ad8b921134c153c077f30fe6e086bcab7b153ce1
eren sezen <erenkaansez17@gmail.com>
2025-09-01 21:48:29 +0300

    addCarAdmınPage

--- CarBooking/Web.UI/Controllers/AdminCarController.cs
+++ CarBooking/Web.UI/Controllers/AdminCarController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Web.Dto.CarDtos;
+using Web.Dto.CarPricingDtos;
 
 namespace Web.UI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7245/api/Cars/GetCarWithBrand");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
