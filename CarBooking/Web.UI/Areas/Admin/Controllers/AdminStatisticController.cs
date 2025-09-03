afa7ff25087d9a49eba6c837e19a5bb293f67d3b
Eren Sezen <erenkaansez17@gmail.com>
2025-09-03 00:56:28 +0000

    add

--- CarBooking/Web.UI/Areas/Admin/Controllers/AdminStatisticController.cs
+++ CarBooking/Web.UI/Areas/Admin/Controllers/AdminStatisticController.cs
@@ -25,65 +25,65 @@ namespace Web.UI.Areas.Admin.Controllers
             var responseCarCount = await client.GetAsync("https://localhost:7245/api/Statistics/GetCarCount");
             if (responseCarCount.IsSuccessStatusCode)
             {
-                var jsonData = await responseCarCount.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.CarCount = values.CarCount;
+                var jsonDataCarCount = await responseCarCount.Content.ReadAsStringAsync();
+                var valuesCarCount = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataCarCount);
+                ViewBag.CarCount = valuesCarCount.CarCount;
             }
             var responseBrandNameByMaxCar = await client.GetAsync("https://localhost:7245/api/Statistics/BrandNameByMaxCar");
             if (responseBrandNameByMaxCar.IsSuccessStatusCode)
             {
-                var jsonData = await responseBrandNameByMaxCar.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.BrandNameByMaxCar = values.BrandNameByMaxCar;
+                var jsonDataBrandNameByMaxCar = await responseBrandNameByMaxCar.Content.ReadAsStringAsync();
+                var valuesBrandNameByMaxCar = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataBrandNameByMaxCar);
+                ViewBag.BrandNameByMaxCar = valuesBrandNameByMaxCar.BrandNameByMaxCar;
             }
             var responseAvgDailyPrice = await client.GetAsync("https://localhost:7245/api/Statistics/GetAvgRentPriceForDaily");
             if (responseAvgDailyPrice.IsSuccessStatusCode)
             {
-                var jsonData = await responseAvgDailyPrice.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.AvgDailyPrice = values.AvgDailyPrice;
+                var jsonDataAvgDailyPrice = await responseAvgDailyPrice.Content.ReadAsStringAsync();
+                var valuesAvgDailyPrice = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataAvgDailyPrice);
+                ViewBag.AvgDailyPrice = valuesAvgDailyPrice.AvgDailyPrice;
             }
             var responseAvgWeeklyPrice = await client.GetAsync("https://localhost:7245/api/Statistics/GetAvgRentPriceForWeekly");
             if (responseAvgWeeklyPrice.IsSuccessStatusCode)
             {
-                var jsonData = await responseAvgWeeklyPrice.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.AvgWeeklyPrice = values.AvgWeeklyPrice;
+                var jsonDataAvgWeeklyPrice = await responseAvgWeeklyPrice.Content.ReadAsStringAsync();
+                var valuesAvgWeeklyPrice = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataAvgWeeklyPrice);
+                ViewBag.AvgWeeklyPrice = valuesAvgWeeklyPrice.AvgWeeklyPrice;
             }
             var responseAvgMonthlyPrice = await client.GetAsync("https://localhost:7245/api/Statistics/GetAvgRentPriceForMonthly");
             if (responseAvgMonthlyPrice.IsSuccessStatusCode)
             {
-                var jsonData = await responseAvgMonthlyPrice.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.AvgMonthlyPrice = values.AvgMonthlyPrice;
+                var jsonDataAvgMonthlyPrice = await responseAvgMonthlyPrice.Content.ReadAsStringAsync();
+                var valuesAvgMonthlyPrice = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataAvgMonthlyPrice);
+                ViewBag.AvgMonthlyPrice = valuesAvgMonthlyPrice.AvgMonthlyPrice;
             }
             var responseBrandCount = await client.GetAsync("https://localhost:7245/api/Statistics/GetBrandCount");
             if (responseBrandCount.IsSuccessStatusCode)
             {
-                var jsonData = await responseBrandCount.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.BrandCount = values.BrandCount;
+                var jsonDataBrandCount = await responseBrandCount.Content.ReadAsStringAsync();
+                var valuesBrandCount = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataBrandCount);
+                ViewBag.BrandCount = valuesBrandCount.BrandCount;
             }
             var responseElectricCarCount = await client.GetAsync("https://localhost:7245/api/Statistics/GetCarCountByElectric");
             if (responseElectricCarCount.IsSuccessStatusCode)
             {
-                var jsonData = await responseElectricCarCount.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.ElectricCarCount = values.ElectricCarCount;
+                var jsonDataElectricCarCount = await responseElectricCarCount.Content.ReadAsStringAsync();
+                var valuesElectricCarCount = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataElectricCarCount);
+                ViewBag.ElectricCarCount = valuesElectricCarCount.ElectricCarCount;
             }
             var responseCarCountByTransmissionIsAuto = await client.GetAsync("https://localhost:7245/api/Statistics/GetCarCountByTransmissionIsAuto");
             if (responseCarCountByTransmissionIsAuto.IsSuccessStatusCode)
             {
-                var jsonData = await responseCarCountByTransmissionIsAuto.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.CarCountByTransmissionIsAuto = values.CarCountByTransmissionIsAuto;
+                var jsonDataCarCountByTransmissionIsAuto = await responseCarCountByTransmissionIsAuto.Content.ReadAsStringAsync();
+                var valuesCarCountByTransmissionIsAuto = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataCarCountByTransmissionIsAuto);
+                ViewBag.CarCountByTransmissionIsAuto = valuesCarCountByTransmissionIsAuto.CarCountByTransmissionIsAuto;
             }
             var responseLocationCount = await client.GetAsync("https://localhost:7245/api/Statistics/GetLocationCount");
             if (responseLocationCount.IsSuccessStatusCode)
             {
-                var jsonData = await responseLocationCount.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
-                ViewBag.LocationCount = values.LocationCount;
+                var jsonDataLocationCount = await responseLocationCount.Content.ReadAsStringAsync();
+                var valuesLocationCount = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonDataLocationCount);
+                ViewBag.LocationCount = valuesLocationCount.LocationCount;
             }
             return View();
         }
